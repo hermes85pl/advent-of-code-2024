d72306e8b5fc902b30135f765346c0f1da5c69c0
Hermes <adam.michal.malecki@gmail.com>
2024-12-09 20:51:30 +0100

    Improve the exectuion script

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+set -eu -o pipefail
+
 TIMEFORMAT='%R'
 DAY=${1:-day*}
 
 rundaypart() {
     if [ -f "$1"/"$2".py ]; then
         echo -n "Running $1 $2... "
-        t=$({ time python -Bbb "$1"/"$2".py <"$1"/input.txt; } 2>&1)
+        t=$({
+            time python -Bbb "$1"/"$2".py <"$1"/input.txt
+            return 0
+        } 2>&1)
         echo "${t}s"
     else
         echo "Missing $1 $2"
@@ -14,6 +19,6 @@ rundaypart() {
 }
 
 for day in $DAY; do
-    rundaypart "$day" part1
-    rundaypart "$day" part2
+    rundaypart "${day}" part1
+    rundaypart "${day}" part2
 done
