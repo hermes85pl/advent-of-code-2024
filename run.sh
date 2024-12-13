2693b811f9e5199873279195bfc5293dd625e1ad
Hermes <adam.michal.malecki@gmail.com>
2024-12-13 16:03:14 +0100

    Improve the exectuion script

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@ set -eu -o pipefail
 
 TIMEFORMAT='%R'
 DAY=${1:-day*}
+FAILURERET=${2:-}
 
 rundaypart() {
     if [ -f "$1"/"$2".py ]; then
         echo -n "Running $1 $2... "
         t=$({
-            time python -Bbb "$1"/"$2".py <"$1"/input.txt
-            return 0
+            time python -Bbb "$1"/"$2".py <"$1"/input.txt || return "${FAILURERET:-$?}"
         } 2>&1)
         echo "${t}s"
     else
