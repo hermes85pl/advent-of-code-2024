de7815c847687b65a586105c8c1509983e09f35f
Hermes <adam.michal.malecki@gmail.com>
2024-12-15 12:55:12 +0100

    Simplify and speed up

--- day14/part2.py
+++ day14/part2.py
@@ -2,7 +2,7 @@ from common import XMAX, XMID, YMAX, YMID, place, robots
 
 
 def distance_from_the_center(pos: tuple[int, int]) -> float:
-    return (abs(pos[0] - XMID) ** 2 + abs(pos[1] - YMID) ** 2) ** 0.5
+    return abs(pos[0] - XMID) + abs(pos[1] - YMID)
 
 
 robots_initially = list(robots())
@@ -12,7 +12,7 @@ answer = None
 for t in range(XMAX * YMAX):
     new_distance = sum(
         distance_from_the_center(place(pos, vel, t))
-        for pos, vel in robots_initially[::3]
+        for pos, vel in robots_initially[::5]
     )
     if min_distance is None or new_distance < min_distance:
         min_distance = new_distance
